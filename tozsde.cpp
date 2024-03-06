450c1a165359fa463d31d12247748ff7f446801d
kzs-666 <137208020+kzs-666@users.noreply.github.com>
2024-03-06 15:40:15 +0100

    Update tozsde.cpp

--- tozsde.cpp
+++ tozsde.cpp
@@ -8,19 +8,21 @@ int main() {
 	string ceg[100], nyeresegesek = "";
 
 	cin >> cegek >> napok;
+
 	for (int i = 0; i < cegek; i++) {
 		cin >> ceg[i] >> zar1;
-		for (int j = 0; j < napok - 1; j++) {
+		for (int j = 0; j < napok - 2; j++) {
 			cin >> t;
 		}
 		cin >> zar2;
 		nyereseg[i] = zar2 - zar1;
+
 		if (nyereseg[i] > max) {
 			max = nyereseg[i];
 			nyeresegesek = ceg[i];
 			db = 1;
 		}
-		if (nyereseg[i] == max) {
+		else if (nyereseg[i] == max) {
 			nyeresegesek += ", " + ceg[i];
 			db++;
 		}

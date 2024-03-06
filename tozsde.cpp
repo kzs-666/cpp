cf9034735e88df6ad292fa50dd95f2a67cf3865f
kzs-666 <137208020+kzs-666@users.noreply.github.com>
2024-03-06 15:37:53 +0100

    Add files via upload

---
+++ tozsde.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int cegek, napok, nyereseg[750], zar1, zar2, t, max = 0, db = 0;
+	string ceg[100], nyeresegesek = "";
+
+	cin >> cegek >> napok;
+	for (int i = 0; i < cegek; i++) {
+		cin >> ceg[i] >> zar1;
+		for (int j = 0; j < napok - 1; j++) {
+			cin >> t;
+		}
+		cin >> zar2;
+		nyereseg[i] = zar2 - zar1;
+		if (nyereseg[i] > max) {
+			max = nyereseg[i];
+			nyeresegesek = ceg[i];
+			db = 1;
+		}
+		if (nyereseg[i] == max) {
+			nyeresegesek += ", " + ceg[i];
+			db++;
+		}
+
+	}
+
+	cout << "#" << endl;
+	cout << max << endl;
+	cout << "#" << endl;
+	cout << db << endl;
+	cout << nyeresegesek << endl;
+
+	return 0;
+}

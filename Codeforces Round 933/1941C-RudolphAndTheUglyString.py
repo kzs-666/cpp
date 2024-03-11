1c4c3b15f5ae6ad33233a5edcc997f8b3394c4b5
kzs-666 <137208020+kzs-666@users.noreply.github.com>
2024-03-11 17:01:56 +0100

    Update 1941C-RudolphAndTheUglyString.py

--- Codeforces Round 933/1941C-RudolphAndTheUglyString.py
+++ Codeforces Round 933/1941C-RudolphAndTheUglyString.py
@@ -2,18 +2,29 @@ test = int(input())
 length = []
 strings = []
 remove = []
+m = False
+p = False
 
 for i in range(0, test):
-    length[i] = int(input())
-    strings[i] = input()
-    remove[i] = 0
+    length.append(int(input()))
+    strings.append(input())
+    remove.append(0)
 
-    while ("map" in strings[i]):
+    if "map" in strings[i]:
+        m = True
+    while (m):
         remove[i] += 1
-        strings[i].replace("map", "xx", 1)
-    while ("pie" in strings[i]):
+        strings[i].replace("map", "00", 1)
+        if "map" not in strings[i]:
+            m = False
+    
+    if "pie" in strings[i]:
+        p = True
+    while (p):
         remove[i] += 1
-        strings[i].replace("pie", "xx", 1)
+        strings[i].replace("pie", "00", 1)
+        if "pie" not in strings[i]:
+            p = False
 
 for i in range(0, test):
     print(remove[i])
